ae53a7fb82b7bf77af805640cd5468d4cd6283b9
MD Anik <122194751+sranik28@users.noreply.github.com>
2023-08-31 13:41:46 +0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Image from 'next/image'
+import React from 'react';
 
-export default function Home() {
+const page = () => {
   return (
-    <main>
-      <h1>Hello World</h1>
-    </main>
-  )
-}
+    <div>
+      <h1>hello world page1</h1>
+    </div>
+  );
+};
+
+export default page;
 
 
